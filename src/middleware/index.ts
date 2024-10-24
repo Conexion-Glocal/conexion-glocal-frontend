a3cd59c26d26930785933bff86096047ab34d2b5
glocal-dev1 <glocal_dev1@umanizales.edu.co>
2024-10-24 11:48:58 -0500

    fix(login and menu): fixed the menu with the new options and login fixed image and size

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,9 +4,9 @@ import jwt from "jsonwebtoken";
 export const onRequest = async (context, next) => {
     const id = context.cookies.get("id").value;
     if (id?.value) {
-        const data = await db.select().from(Session).where(eq(Session.id, id))
+        const data = await db.select().from(Session).where(eq(Session.id, id));
         const accessToken = jwt.verify(data[0].access_token, "secretkey");
-        const refreshToken = jwt.verify(data[0].refresh_token, 'secretkey')
+        const refreshToken = jwt.verify(data[0].refresh_token, 'secretkey');
         if (accessToken) {
             if (PUBLIC_ROUTES.includes(context.url.pathname) || context.url.pathname.search('/password/reset/') != -1) {
                 return Response.redirect(`${context.url.origin}/home`);
@@ -21,4 +21,5 @@ export const onRequest = async (context, next) => {
             }
         }
     }
+    return next();
 }
